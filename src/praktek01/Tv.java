e74c07e7155232625e74117e93a97eda36787f2a
sri <sri12072>
2018-04-06 21:35:06 +0800

    membuat atribut dan behavior

--- src/praktek01/Tv.java
+++ src/praktek01/Tv.java
@@ -10,5 +10,18 @@ package praktek01;
  * @author My PC
  */
 public class Tv {
+    string merk;
+    string harga;
+    string ukuran;
+    string kualitas;
+    string garansi;
+    
+    void cetakinfo(){
+        system.out.println("merk"+merk);
+        system.out.println("harga"+harga);
+        system.out.println("ukuran"+ukuran);
+        system.out.println("kualitas"+kualitas);
+        system.out.println("garansi"+garansi);
+    }
     
 }
